4d91e4cf4e13d72a458e76cf83e41c1006e1b4b1
Rohit <rkumar8092378845@gmail.com>
2023-10-12 20:24:02 +0530

    Initial commit 12-10-2023

---
+++ src/main/java/com/rkumar0206/mymemailandnotificationservice/MymEmailAndNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rkumar0206.mymemailandnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymEmailAndNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymEmailAndNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkumar0206/mymemailandnotificationservice/MymEmailAndNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkumar0206.mymemailandnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymEmailAndNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

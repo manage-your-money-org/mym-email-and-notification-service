43e4681dc1f88153fbd595f30d50955b3de57162
Rohit <rkumar8092378845@gmail.com>
2023-10-17 12:36:38 +0530

    - minor change

--- src/test/java/com/rkumar0206/mymemailandnotificationservice/MymEmailAndNotificationServiceApplicationTests.java
+++ src/test/java/com/rkumar0206/mymemailandnotificationservice/MymEmailAndNotificationServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.rkumar0206.mymemailandnotificationservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MymEmailAndNotificationServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class MymEmailAndNotificationServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
